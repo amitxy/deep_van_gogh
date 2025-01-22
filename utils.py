d621966ef530f1f94b9d39bda267f60e8a00a86f
amitxy <amitxy96@gmail.com>
2025-01-22 11:08:27 +0200

    paths are now in utils.py

--- utils.py
+++ utils.py
@@ -1,2 +1,17 @@
 import os
-pass
+import time
+import sys
+
+ROOT = './data/dataset'
+DATASET_DIR = 'data/dataset/Post_Impressionism'
+CSV_PATH = 'data/dataset/classes.csv'
+OPTIMIZED_DIR = './data/optimized'
+
+
+def show_optimization_progress(current_size, total_size):
+    sys.stdout.flush()
+    percent = (current_size / total_size) * 100
+    if percent == 100:
+        print(f"\rOptimizing dataset... {percent:.2f}%")
+    else:
+        sys.stdout.write(f"\rOptimizing dataset... {percent:.2f}%")
